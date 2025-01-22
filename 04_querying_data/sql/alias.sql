4d1a8e67b296a083bdf8a6078a875b3e57dcf8b1
Charlotta <charlotta.dellborg@hotmail.se>
2025-01-22 12:40:50 +0100

    scripts from codealong "Querying data in DuckDB"

---
+++ 04_querying_data/sql/alias.sql
@@ -0,0 +1,14 @@
+SELECT
+	DISTINCT salary_currency AS unique_currency
+FROM
+	main.data_jobs;
+
+SELECT
+	COUNT(DISTINCT salary_currency) AS number_currencies
+FROM
+	main.data_jobs;
+
+SELECT
+	COUNT(DISTINCT salary_currency) number_currencies
+FROM
+	main.data_jobs;
